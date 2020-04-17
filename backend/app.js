fa3e6461727bdcfd970e3faf9f704ca653e7003b
Sky020 <2397860h@student.gla.ac.uk>
2020-04-17 23:51:48 +0100

    Added export functions

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+const URL = "https://potterswheelsd.org/sermons/";
 // ----------------------------------------------------------------------
 // Setup Mongoose Schema connection
 // ----------------------------------------------------------------------
@@ -53,18 +55,6 @@ function findSermon() {
     };
 }
 
-function updateDB() {
-    // Find last added sermon (findbyDate)
-
-    // Check for more sermons, and save new sermons
-    const createManySermons = function (arrayOfSermons, done) {
-        Sermon.create(arrayOfSermons, function (err, sermon) {
-            if (err) return console.error("4 ", err);
-            done(null, sermon);
-        });
-    };
-}
-
 function CreateSermonDB() {
 
     let pages = [];
@@ -76,12 +66,12 @@ function CreateSermonDB() {
     let audios = [];
 
     function getPages() {
-        axios(url)
+        axios(URL)
             .then((html) => {
                 const $ = cheerio.load(html.data)
                 let numberOfPages = $('#cmsmasters_sermons_5e960f113c712 > div > div > ul > li:nth-child(5) > a').text()
                 for (let i = 1; i < numberOfPages + 1; i++) {
-                    pages.push(url + 'page/' + String(i));
+                    pages.push(URL + 'page/' + String(i));
                 }
                 getAllData()
             })
@@ -113,7 +103,7 @@ function CreateSermonDB() {
         })
     }
 
-    const createMany = async () => {
+    const createMany = () => {
         let sermonData = [];
         for (let i = 0; i < sermons.length; i++) {
             sermonData.push({
@@ -127,9 +117,10 @@ function CreateSermonDB() {
         let arrayOfSermons = sermonData.map((sermon) => {
             return { title: sermon.title, speaker: sermon.speaker, date: sermon.date, category: sermon.category, audio: sermon.audio, seen: false }
         });
-        createManySermons(arrayOfSermons)
+        createManySermons(arrayOfSermons, finishedFunc)
+        function finishedFunc(nil, data) { console.log("FINISHED CALLED") }
 
-        const createManySermons = function (arrayOfSermons, done) {
+        const createManySermons = (arrayOfSermons, done) => {
             Sermon.create(arrayOfSermons, function (err, sermon) {
                 if (err) return console.error(err);
                 done(null, sermon);
@@ -138,3 +129,65 @@ function CreateSermonDB() {
     }
 
 }
+
+//----------------------------------------------------------------------------------
+// Functions to export
+//----------------------------------------------------------------------------------
+async function getAllSermons() {
+    return await Sermon.find();
+}
+
+async function updateDB() {
+    let pages = [];
+    let sermons = [];
+    let titles = [];
+    let speakers = [];
+    let categories = [];
+    let dates = [];
+    let audios = [];
+    // Find last added sermon (findbyDate)
+    let mostRecent;
+    Sermon.findOne().sort({ created_at: -1 }).exec(function (err, data) {
+        if (err) return console.log(err);
+        mostRecent = data;
+    });
+    // Check for more sermons, and save new sermons
+    const findSermonsAfterDate = (mostRecent) => {
+        axios(URL)
+            .then(html => {
+                const $ = cheerio.load(html.data);
+                let refs = $('article > h3 > a');
+                let tempDates = $('article > abbr');
+                let tempAudios = $('a.cmsmasters_sermon_media_item.cmsmasters_theme_icon_sermon_download');
+                let tempSpeakers = $('div.cmsmasters_sermon_author > a > span');
+                let tempCategories = $('div.cmsmasters_sermon_cat > a');
+                for (let ref in refs) {
+                    if (!isNaN(ref)) {
+                        sermons.push(refs[ref].attribs.href)
+                        titles.push(refs[ref].children[0].data)
+                        audios.push(tempAudios[ref].attribs.href)
+                        dates.push(tempDates[ref].attribs.title)
+                        speakers.push(tempSpeakers[ref].children.data)
+                        categories.push(tempCategories[ref].children.data)
+                    }
+                }
+            }).then(createMany())
+            .catch(console.error)
+    }
+    const createManySermons = function (arrayOfSermons, done) {
+        Sermon.create(arrayOfSermons, function (err, sermon) {
+            if (err) return console.error("4 ", err);
+            done(null, sermon);
+        });
+    };
+}
+
+//----------------------------------------------------------------------------------
+// Exports
+//----------------------------------------------------------------------------------
+
+// getAllSermons
+export default getAllSermons;
+// updateSermons
+export default updateDB;
+//
